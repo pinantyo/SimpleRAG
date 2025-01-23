ccd7a68ece105edb676fa82dcce7ac41dceffad5
pinantyo <stevenchristpa2001@gmail.com>
2025-01-23 12:17:19 +0700

    kerangka awal chat message

--- src/app.py
+++ src/app.py
@@ -1,5 +1,49 @@
 import streamlit as st
 
-st.write("Hi")
+
+st.markdown("""
+    <style>
+            .block-container{
+                max-width:85%!important;
+            }
+    </style>
+""", unsafe_allow_html=True)
+
+def page1():
+    st.write(st.session_state.foo)
+
+def page2():
+    st.write(st.session_state.bar)
+
+# Widgets shared by all the pages
+with st.sidebar:
+    st.button(
+        "Start new message",
+        on_click = None,
+        icon = None,
+        disabled=False,
+        use_container_width=True,
+        type='secondary'
+    )
 
 
+    st.navigation(
+        [st.Page(page1, title="Hi"), st.Page(page2, title="Hi2")],
+        position='sidebar',
+        expanded=False
+    )
+
+with st.container():
+    messages = st.container(height=700, border=False)
+    if prompt := st.chat_input(
+        placeholder = "Your message",
+        max_chars = None,
+        disabled = False,
+        on_submit = None,
+    ):
+        messages.chat_message("user").write(prompt)
+        messages.chat_message("assistant").write(f"Echo: {prompt}")
+
+
+
+    
