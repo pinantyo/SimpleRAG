558cebee2c17489fc6020910ced9edb6d17fc880
pinantyo <stevenchristpa2001@gmail.com>
2025-02-19 12:06:54 +0700

    db

--- data/engine.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from data import models
-import os
-
-# create an engine
-current_path = os.path.dirname(os.path.realpath(__file__))
-database_url = f"sqlite+pysqlite:///{current_path}/database.db"
-engine = create_engine(database_url, echo=True)
